d17c70495986d455a8704df4e2a4a75520737c2d
SADIYA3 <146457920+SADIYA3@users.noreply.github.com>
2025-06-05 10:16:21 +0530

    Add files via upload

---
+++ Assetmanagement3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assetmanagement3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assetmanagement3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
